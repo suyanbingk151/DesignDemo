4dc83a0902fdb33630cefbe2cde001b0321964c5
苏利晨 <sulc@gionee.com>
2017-02-24 14:12:51 +0800

    yes

--- app/src/main/java/com/suxx/gionee/designdemo/SecondActivity.java
+++ app/src/main/java/com/suxx/gionee/designdemo/SecondActivity.java
@@ -1,8 +1,14 @@
 package com.suxx.gionee.designdemo;
 
+import android.util.Log;
+
 /**
  * Created by gionee on 2017/2/24.
  */
 public class SecondActivity {
+
+  public SecondActivity(){
+
+  }
 
 }
